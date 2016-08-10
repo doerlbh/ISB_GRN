9630e9ac6d542cba7d1f8d97bdb55547451ac374
doerlbh <doerlbh@gmail.com>
2016-08-10 14:42:05 -0700

    update code

--- GRN_N_genes_read.r
+++ GRN_N_genes_read.r
@@ -62,7 +62,7 @@
 rm(list=ls())
 require(deSolve) # load the ode package
 
-set.seed(34)
+set.seed(1234)
 Ipath="./data_20160807/"
 Opath="./data_20160807/"
 
@@ -71,186 +71,114 @@ pert = 20    # time steps of perturbation
 num = 2
 
 # for (i in 1:num) {
-#SS <- read.table(paste("N9-X", i,"-SS.txt",sep=""), header=F)
-#PP <- read.table(paste("N9-X", i,"-Para.txt",sep=""), header=F)
+#SS = as.matrix(read.table(paste("N9-X", i,"-SS.txt",sep=""), header=F))
+#PP = as.matrix(read.table(paste("N9-X", i,"-Para.txt",sep=""), header=F))
 #}
-SS <- read.table(paste("N9-X1-SS.txt",sep=""), header=F)
-PP <- read.table(paste("N9-X1-Para.txt",sep=""), header=F)
+SS = as.matrix(read.table(paste("N9-X1-SS.txt",sep=""), header=F))
+PP = as.matrix(read.table(paste("N9-X1-Para.txt",sep=""), header=F))
+P = PP[1:length(PP[1,]), 1:length(PP[1,])]
+A = 100*diag(P);
+M = PP[length(PP[1,])+1, 1:length(PP[1,])]
+N = PP[length(PP[1,])+2, 1:length(PP[1,])]
 
-# Start writing to an output file
-sink(paste("N",n,"-X",p,".txt",sep=""))
-
-cat(sprintf("Network %d with %d nodes has %d states:\n", p, n, ssc))
-cat("=============================\n")
-cat("Network parameters:\n")
-cat("\n P is \n")
-
-for(node in 1:n) {
-  cat(P[i,]);
-}
-
-cat("\n M is \n")
-cat(M);
-
-cat("\n A is \n")
-cat(A);
-
-cat("\n N is \n")
-cat(N)
-
-cat("\n Network steady states: \n")    
-
-for(j in 1:ssc) {
-  cat(P[,j]);
-}
-
-cat("=============================\n")
-
-# Stop writing to the file
-sink()
-
-
-
-
-num = 2      # network number 
-trial = 50  # trial
-trshd = 0.0001 # threshold for different states
-sstrshd = 1e-8       # threshold for equilibrium of steady states
-
-Nstate = mat.or.vec(num,1)  # store how many states each network can have
-
-# Starting values
-randset = 1000  # range of 
-Prandset = 10000 # range of parameters
-
-x0 = mat.or.vec(trial,n)   # startvalues (genes)
-x0 = randset*matrix(round(runif(trial*n),randset), trial, n) 
-
-temp1 = mat.or.vec(n,1)
-temp2 = mat.or.vec(n,1)
-
-# Direct parameter declaration:
-x = mat.or.vec(n,1)   # gene number
-A = mat.or.vec(n,n)   # gene sythesis rate
-M = mat.or.vec(n,1)   # gene degradation rate
-P = mat.or.vec(n,n)   # parameters, i.e. gene-gene interaction (repress or activate) 
-N = mat.or.vec(n,n)   # interaction types
-
-p = 1;
-while (p <= num) {
-  Nss = NULL      # store all the steady states 
-  ssc = 1;
-  
-  P = Prandset*matrix(round(runif(n*n),Prandset), n, n) 
-  M = matrix(round(runif(n*1), Prandset),n,1)
-  A = 100*diag(P)
-  
+# ODE function
+func <- function(t,xx,p)
+{
+  dx = mat.or.vec(n,1)
+  #print(length(xx))
+  x = xx  
   for (node in 1:n) {
-    N[node,] = sample(c(1, 0,-1),n,TRUE)
-    N[node,node] = 0
-    #print(N)
+    temp1 = (N>0)*P[node,]*x
+    temp1[temp1 == 0] = 1
+    temp2 = (N<0)*P[node,]*x
+    temp2[temp2 == 0] = 1
+    dx[node] = A[node]*prod(temp1)/(prod(1+temp1)*prod(1+temp2)) - M[node]*x[node]
   }
   
-  
-  # ODE function
-  func <- function(t,xx,p)
-  {
-    dx = mat.or.vec(n,1)
-    #print(length(xx))
-    x = xx  
-    for (node in 1:n) {
-      temp1 = (N>0)*P[node,]*x
-      temp1[temp1 == 0] = 1
-      temp2 = (N<0)*P[node,]*x
-      temp2[temp2 == 0] = 1
-      dx[node] = A[node]*prod(temp1)/(prod(1+temp1)*prod(1+temp2)) - M[node]*x[node]
-    }
-    
-    list(dx)    # give the change rates to the solver
-  }
-  
-  # solve ODE
-  ph = 100
-  parms = c()          # parameter (if necesarry)
-  times = seq(0,ph,0.1) 
-  xr = array(rep(1, trial*n*length(times)), dim=c(trial,n,length(times)))
-  
-  t = 1;
-  while (t <= trial) {
-    #print("t")
-    #print(t);
-    x0r <- x0[t,]
-    res <- lsoda(x0r,times, func, parms)   # solve it
-    res <- as.data.frame(res)             # make a data frame
-    for (node in 1:n) {
-      xr[t,node,] <- res[,node+1]  
-    }
-    #print(dim(res))
-    oldt = length(times);
-    pht = 2*ph;
-    timest = seq(0,pht,0.1);
-    xrt = array(rep(1, trial*n*length(timest)), dim=c(trial,n,length(timest)));
-    xrt[,,1:oldt] = xr[,,1:oldt];
-    for (trialt in 1:t){
-      for (tt in oldt:length(timest)) {
-        for (node in 1:n) {
-          xrt[trialt,node,tt]=xr[trialt,node,oldt];
-        }
-      }
-    }
-    if (mean(abs(xrt[t,,length(times)]-xrt[t,,length(times)-1])) > sstrshd) {
-      ph = pht;
-      xr = xrt; 
-      times = timest;
-      t = t - 1;
-    }
-    
-    t = t + 1;
-    #print(ph)
-  }
-  
-  # plot gene time profiles                                                       
-  #graphics.off()
-  #windows(xpos=1,ypos=-50,width=n,height=4)
-  
-  Nss = cbind(Nss, xr[1,,length(xr)/(n*trial)]);
-  for (i in 1:trial){
-    for (j in 1:round(length(Nss)/node)) {
-      if (mean(abs(xr[i,,length(xr)/(n*trial)] - Nss[,j]))>trshd) {
-        ssc = ssc + 1;
-        Nss = cbind(Nss, xr[i,,length(xr)/(n*trial)]);        
-      }
-    }
-  }
-  
-  if (ssc == 1) {
-    p = p - 1;
-  } else {
-    maxY = max(xr)
-    #maxY = max( c(max(x1r),max(x2r),max(x3r)))
-    
-    png(filename = paste(Opath,"N", n, "-T", trial,"-P",p, ".png", sep=""), 
-        width = 480, height = 480, 
-        units = "px", pointsize = 12, bg = "white")
-    
-    plot(times,xr[1,1,],ylim=c(0, maxY), main=paste("N",n,"-T",trial,"-Trajectory",sep=""),
-         type="l",xlab="t",ylab="x",lwd=2,col=rgb(0,0,1/n))
-    #legend("topleft", lty=1:1)
-    
-    for (i in 1:trial){
-      for (node in 1:n) {
-        lines(times,xr[i,node,],lwd=2,col=rgb(0,0,node/n));
-      }
-    }
-    
-    mtext(paste(n,"_gene_",ssc, "_state_network_#", p, sep=""))
-    
-    dev.off()
-    
-    
-  }
-  p = p + 1;
+  list(dx)    # give the change rates to the solver
 }
+
+# solve ODE
+ph = 100
+parms = c()          # parameter (if necesarry)
+times = seq(0,ph,0.1) 
+xr = array(rep(1, trial*n*length(times)), dim=c(trial,n,length(times)))
+
+t = 1;
+while (t <= trial) {
+  #print("t")
+  #print(t);
+  x0r <- x0[t,]
+  res <- lsoda(x0r,times, func, parms)   # solve it
+  res <- as.data.frame(res)             # make a data frame
+  for (node in 1:n) {
+    xr[t,node,] <- res[,node+1]  
+  }
+  #print(dim(res))
+  oldt = length(times);
+  pht = 2*ph;
+  timest = seq(0,pht,0.1);
+  xrt = array(rep(1, trial*n*length(timest)), dim=c(trial,n,length(timest)));
+  xrt[,,1:oldt] = xr[,,1:oldt];
+  for (trialt in 1:t){
+    for (tt in oldt:length(timest)) {
+      for (node in 1:n) {
+        xrt[trialt,node,tt]=xr[trialt,node,oldt];
+      }
+    }
+  }
+  if (mean(abs(xrt[t,,length(times)]-xrt[t,,length(times)-1])) > sstrshd) {
+    ph = pht;
+    xr = xrt; 
+    times = timest;
+    t = t - 1;
+  }
+  
+  t = t + 1;
+  #print(ph)
+}
+
+# plot gene time profiles                                                       
+#graphics.off()
+#windows(xpos=1,ypos=-50,width=n,height=4)
+
+Nss = cbind(Nss, xr[1,,length(xr)/(n*trial)]);
+for (i in 1:trial){
+  for (j in 1:round(length(Nss)/node)) {
+    if (mean(abs(xr[i,,length(xr)/(n*trial)] - Nss[,j]))>trshd) {
+      ssc = ssc + 1;
+      Nss = cbind(Nss, xr[i,,length(xr)/(n*trial)]);        
+    }
+  }
+}
+
+if (ssc == 1) {
+  p = p - 1;
+} else {
+  maxY = max(xr)
+  #maxY = max( c(max(x1r),max(x2r),max(x3r)))
+  
+  png(filename = paste(Opath,"N", n, "-T", trial,"-P",p, ".png", sep=""), 
+      width = 480, height = 480, 
+      units = "px", pointsize = 12, bg = "white")
+  
+  plot(times,xr[1,1,],ylim=c(0, maxY), main=paste("N",n,"-T",trial,"-Trajectory",sep=""),
+       type="l",xlab="t",ylab="x",lwd=2,col=rgb(0,0,1/n))
+  #legend("topleft", lty=1:1)
+  
+  for (i in 1:trial){
+    for (node in 1:n) {
+      lines(times,xr[i,node,],lwd=2,col=rgb(0,0,node/n));
+    }
+  }
+  
+  mtext(paste(n,"_gene_",ssc, "_state_network_#", p, sep=""))
+  
+  dev.off()
+  
+  
+}
+
 
 
 
