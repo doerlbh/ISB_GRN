fe437d703805f799e18445fbe8f74eda0d9060d2
doerlbh <doerlbh@gmail.com>
2016-08-18 16:06:57 -0700

    update code

--- GRN_N_Net.m
+++ GRN_N_Net.m
@@ -9,13 +9,13 @@ close all;
 
 rng(123);                 % randomizer
 
-pathN = '/Users/DoerLBH/Dropbox/git/ISB_GRN/data/M-20160818/';
+pathN = '/Users/DoerLBH/Dropbox/git/ISB_GRN/data/Mtest3-20160818/';
 system(['mkdir ' pathN]);
 
-num = 5;         % network number
+num = 10;         % network number
 n = 4;           % gene
-trial = 50;      % trial
-trshd = 1;       % threshold for different states
+trial = 20;      % trial
+ddtrshd = 1;       % threshold for different states
 sstrshd = 1;     % threshold for equilibrium of steady states
 tend = 3000;     % threshold for equilibrium vs. non-equlibrium
 zthrs = 1;       % threshold for non-trivial states
@@ -38,8 +38,8 @@ P = zeros(n,n);   % parameters, i.e. gene-gene interaction (repress or activate)
 
 p = 1;           % num of generated networks (starting 1)
 
-count = 1;
-% parfor count = 1:num
+% count = 1;
+parfor count = 1:num
     
     P = Pr*rand(n, n);
     M = rand(n,1);
@@ -63,14 +63,25 @@ count = 1;
         if ss == 1
             ssn = y(size(y,1),:);
             if ~prod((abs(ssn)<zthrs))
-                Nss = vertcat(Nss, ssn);
                 
-                fig = figure;
-                plot(y);
-                title(strcat('X',num2str(count),'-N',num2str(n),'-T',num2str(t)));
-                filename = strcat(pathN, 'X',num2str(count),'-N',num2str(n),'-T',num2str(t),'.png');
-                parsaveas(gcf, filename,'png');
-                close gcf;
+                if size(Nss,1) > 0
+                    for st = 1:size(Nss,1)
+                        samess(count) = prod((Nss(st,:)-ssn)<ddtrshd);
+                    end
+                else
+                    samess(count) = 0;
+                end
+                
+                if ~samess(count)
+                    Nss = vertcat(Nss, ssn);
+                    
+                    fig = figure;
+                    plot(y);
+                    title(strcat('X',num2str(count),'-N',num2str(n),'-T',num2str(t)));
+                    filename = strcat(pathN, 'X',num2str(count),'-N',num2str(n),'-T',num2str(t),'.png');
+                    parsaveas(gcf, filename,'png');
+                    close gcf;   
+                end
             end
         end
     end
@@ -83,12 +94,12 @@ count = 1;
         fname = strcat('UX',num2str(count),'-N',num2str(n),'-C',num2str(ssc));
     end
     
-    parsave(strcat(pathN, fname, '-Nss.txt'), 'Nss', '-ascii');
-    parsave(strcat(pathN, fname, '-P.txt'), 'P', '-ascii');
-    parsave(strcat(pathN, fname, '-A.txt'), 'A', '-ascii');
-    parsave(strcat(pathN, fname, '-N.txt'), 'N', '-ascii');
-    parsave(strcat(pathN, fname, '-M.txt'), 'M', '-ascii');
+    parsave(strcat(pathN, fname, '-Nss.txt'), Nss, '-ascii');
+    parsave(strcat(pathN, fname, '-P.txt'), P, '-ascii');
+    parsave(strcat(pathN, fname, '-A.txt'), A, '-ascii');
+    parsave(strcat(pathN, fname, '-N.txt'), N, '-ascii');
+    parsave(strcat(pathN, fname, '-M.txt'), M, '-ascii');
     
-% end
+end
 
 
