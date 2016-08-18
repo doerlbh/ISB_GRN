560e3860e4bcd119ecaa1f1d53800d7061f82c0f
doerlbh <doerlbh@gmail.com>
2016-08-18 11:45:31 -0700

    update code

---
+++ netode.m
@@ -0,0 +1,8 @@
+function [fACor] = netode(stP, ftrial, fAutoT, fpathN, fa, fL, fangle, fHc, fHt, fname)
+% ode function used to generate networks
+
+
+
+save(strcat(fpathN, fname, '.txt'), 'fACor', '-ascii');
+
+end
